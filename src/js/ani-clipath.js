5daa515c224e40746ddadb54f61cb6e277aa57e9
Jesse Luo <luo_sj@163.com>
2017-09-27 02:17:13 +0800

    add function

--- src/js/ani-clipath.js
+++ src/js/ani-clipath.js
@@ -1,14 +1,25 @@
 const defaultShapes = [
-
+  [
+    {
+      c: '#000000',
+      p: [ { x: '0%', y: '50%' }, { x: '50%', y: '0%' }, { x: '100%', y: '100%' }]
+    }
+  ],
+  [
+    {
+      c: '#000000',
+      p: [ { x: '0%', y: '0%' }, { x: '50%', y: '0%' }, { x: '0%', y: '50%' }]
+    }
+  ]
 ]
 
-class aniClipath () {
-  constructor () {
-    
-  }
-  buildNodes () {
-    
+class AniClipath {
+  constructor ( el, shapes ) {
+    if (!el) {
+      throw error('A DOM container is required!')
+    }
+    this.shapes = shapes || defaultShapes
   }
 }
 
-export default aniClipath
+export default AniClipath
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
 import '../scss/main.scss'
+import animals from './animals.js'
+import AniClipath from './ani-clipath.js'
+
+// -------------------------------------------------------- //
+
+animals.forEach( elem => {
+  elem.forEach( _elem => {
+  	_elem.p.forEach( __elem => {
+  	  __elem.x = (__elem.x/3200*100).toFixed(2) + '%'
+  	  __elem.y = (__elem.y/2400*100).toFixed(2) + '%'
+  	})
+  })
+})
 
